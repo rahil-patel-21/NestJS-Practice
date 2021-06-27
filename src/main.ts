40dc5d5e9c3883a420873f0d3f0b08b9546cbaf6
TechAddict <rahil.geek21@outlook.com>
2021-06-27 07:45:35 +0530

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/v1');
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
