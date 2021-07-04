d8214271ebf0c50938086bf283310fdd44bd84c6
TechAddict <rahil.geek21@outlook.com>
2021-07-04 18:11:48 +0530

    firebase initialized

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,22 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidatePipe } from './core/pipes/validator.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  admin.initializeApp({
+    credential: admin.credential.cert(
+      'nestjs-practice-firebase-adminsdk-m59s7-ce021fec5d.json',
+    ),
+  });
   app.setGlobalPrefix('/v1');
   const config = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidatePipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
